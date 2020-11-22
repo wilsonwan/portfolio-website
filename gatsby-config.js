c045806fbd85d45493d02fac4d0837615e01ca8d
Wilson Wan <waiwilsonwan@gmail.com>
2020-11-22 16:22:24 +0000

    clear repo

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    siteMetadata: {
-        "currentJobTitle": "Software Engineer",
-        "personalSummary": "Software engineer with 2 years of professional experience, have a strong passion for software engineering. Confident communicator, strategic planner, strong team member but capable leading projects from requirement analysis to implementation."
-      },
-    plugins: [
-        'gatsby-plugin-typescript'
-    ]
-}
